8b4bdae026b7d6ec4574250f93be7c289da465c2
Jung hwan Kim (Nathan) <nathanjungkim.dev@gmail.com>
2025-08-05 23:52:32 -0700

    feat: add getComputerChoice and getHumanChoice with pseudocode[200~

--- Script.js
+++ Script.js
@@ -1,3 +1,15 @@
+/*
+1.FUNCTION getComputerChoice
+  GENERATE random number between 0 and 2
+  IF number is 0 THEN
+    RETURN "rock"
+  ELSE IF number is 1 THEN
+    RETURN "paper"
+  ELSE
+    RETURN "scissors"
+END FUNCTION
+*/
+
 function getComputerChoice() {
   let randomnum = Math.floor(Math.random() * 3);
   if (randomnum === 0) {
@@ -8,6 +20,16 @@ function getComputerChoice() {
     return "scissors";
   }
 }
+
+/*
+2.FUNCTION getHumanChoice
+  PROMPT user to enter "rock", "paper", or "scissors"
+  STORE user's input as choice
+  REMOVE whitespace from the beginning and end of choice
+  CONVERT choice to lowercase
+  RETURN choice
+END FUNCTION
+*/
 
 function getHumanChoice() {
   let choice = prompt("rock, paper, or scissors?");
