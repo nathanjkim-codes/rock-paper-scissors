4c238dd87dfea67bd364ef274361e98c6f7343a4
Jung hwan Kim (Nathan) <nathanjungkim.dev@gmail.com>
2025-08-06 20:37:06 -0700

    feat(game): add playRound logic and declare score variables
    - Compare player and computer choices
    - Increment scores based on round outcome
    - Normalize human input to lowercase

--- Script.js
+++ Script.js
@@ -1,5 +1,5 @@
 /*
-1.FUNCTION getComputerChoice
+Step 1: FUNCTION getComputerChoice
   GENERATE random number between 0 and 2
   IF number is 0 THEN
     RETURN "rock"
@@ -22,7 +22,7 @@ function getComputerChoice() {
 }
 
 /*
-2.FUNCTION getHumanChoice
+Step 2: FUNCTION getHumanChoice
   PROMPT user to enter "rock", "paper", or "scissors"
   STORE user's input as choice
   REMOVE whitespace from the beginning and end of choice
@@ -38,3 +38,33 @@ function getHumanChoice() {
 
 console.log("Computer chose:", getComputerChoice());
 console.log("Human chose:", getHumanChoice());
+
+/*
+Step 4: Declare the players score variables
+*/
+
+let humanScore = 0;
+let computerScore = 0;
+
+/*
+Step 5: Compare human and computer choice  
+If same → tie  
+If human wins → human score +1  
+Else → computer score +1
+*/
+
+function getComputerChoice() {
+  let randomnum = Math.floor(Math.random() * 3);
+  if (randomnum === 0) {
+    return "rock";
+  } else if (randomnum === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+function getHumanChoice() {
+  let choice = prompt("rock, paper, or scissors?");
+  return choice.trim().toLowerCase();
+}
